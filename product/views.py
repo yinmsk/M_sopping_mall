609320d3e7d35809fe3d89c9f0d57954efb3c5eb
yinmsk <galaxy220111@gmail.com>
2023-02-22 00:07:06 +0900

    fest(product): views.py에서 작성할 기본 crud 뼈대 작성

--- product/views.py
+++ product/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import permissions, status
+
+
+class ProductView(APIView):  # CBV 방식
+    permission_classes = [permissions.AllowAny]  # 누구나 view 조회 가능
+    # permission_classes = [permissions.IsAdminUser] # admin만 view 조회 가능
+    # permission_classes = [permissions.IsAuthenticated] # 로그인 된 사용자만 view 조회 가능
+
+    def get(self, request):
+        return Response({'message': 'get method!!'})
+
+    def post(self, request):
+        return Response({'message': 'post method!!'})
+
+    def put(self, request):
+        return Response({'message': 'put method!!'})
+
+    def delete(self, request):
+        return Response({'message': 'delete method!!'})
